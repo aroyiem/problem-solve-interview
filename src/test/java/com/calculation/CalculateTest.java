a4091fb0d7197c16b3c9307ad9362a7ae6bc20a5
Anirban Roy <aroyiem@gmail.com>
2021-01-13 17:40:13 +0000

    Update CalculateTest.java

--- src/test/java/com/calculation/CalculateTest.java
+++ src/test/java/com/calculation/CalculateTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 public class CalculateTest extends TestCase {
 
+
     public CalculateTest(String name) {
         super(name);
     }
@@ -19,9 +20,69 @@ public class CalculateTest extends TestCase {
         Assert.assertEquals(4.0d, calculateSquare.getArea());
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void testCalculateAreaForSquare_nullobject() {
-        Calculate calculateSquare = new CalculateForSquare(null);
-        Double area = calculateSquare.getArea();
+        try {
+            Calculate calculateSquare = new CalculateForSquare(null);
+            Double area = calculateSquare.getArea();
+            fail("test case broken");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Incomplete information");
+        }
+
+    }
+
+    @Test
+    public void testCalculateTotalDistanceForSquare_success() {
+        Calculate calculateSquare = new CalculateForSquare(new Square(2.0));
+        Assert.assertEquals(8.0d, calculateSquare.getTotalDistance());
+    }
+
+    @Test
+    public void testCalculateTotalDistanceForSquare_nullobject() {
+        try {
+            Calculate calculateSquare = new CalculateForSquare(null);
+            Double totalDistance = calculateSquare.getTotalDistance();
+            fail("test case broken");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Incomplete information");
+        }
+
+    }
+
+    @Test
+    public void testNumberOfSidesForSquare_success() {
+        Calculate calculateSquare = new CalculateForSquare(new Square(2.0));
+        Assert.assertEquals(4, (int)calculateSquare.getNumberOfSides());
+    }
+
+    @Test
+    public void testNumberOfSidesForSquare_nullobject() {
+        try {
+            Calculate calculateSquare = new CalculateForSquare(null);
+            Integer numberOfSide = calculateSquare.getNumberOfSides();
+            fail("test case broken");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Incomplete information");
+        }
+
+    }
+
+    @Test
+    public void testOtherAttributesForSquare_success() {
+        Calculate calculateSquare = new CalculateForSquare(new Square(2.0));
+        Assert.assertNull(calculateSquare.getOtherAttributes());
+    }
+
+    @Test
+    public void testOtherAttributesForSquare_nullobject() {
+        try {
+            Calculate calculateSquare = new CalculateForSquare(null);
+            String attributes = calculateSquare.getOtherAttributes();
+            fail("test case broken");
+        } catch (RuntimeException e) {
+            assertEquals(e.getMessage(), "Incomplete information");
+        }
+
     }
 }
